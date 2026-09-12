57ba167c1e22af02749715fdd0d86508cf6a7ef8
mdsulemannadeem <85692443+mdsulemannadeem@users.noreply.github.com>
2026-09-12 19:55:24 +0530

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.java
@@ -0,0 +1,33 @@
+class NumMatrix {
+  private int[][] prefix;
+    public NumMatrix(int[][] matrix) {
+         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+           prefix = new int[rows + 1][cols + 1];
+
+             for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                prefix[r + 1][c + 1] = matrix[r][c] 
+                                     + prefix[r][c + 1] 
+                                     + prefix[r + 1][c] 
+                                     - prefix[r][c]; // Overlap ko subtract kiya
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2 + 1][col2 + 1] 
+             - prefix[row1][col2 + 1] 
+             - prefix[row2 + 1][col1] 
+             + prefix[row1][col1];
+
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
